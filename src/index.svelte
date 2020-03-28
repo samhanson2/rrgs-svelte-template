6fbf83c507297dd9dfafeb0c8728070389522a38
Samantha <hanson.s@rangiruru.school.nz>
2020-03-29 10:48:17 +1300

    Hello world And The Repeater
    Added code to make Hello World and also added code the repeater

--- src/index.svelte
+++ src/index.svelte
@@ -1,3 +1,24 @@
+<script>
+let name ='World'
+let text =''
+</script>
+
+<style>
+</style>
 <h1>Svelte</h1>
 
 <p>Welcome to coding with Svelte!</p>
+<br>
+<br>
+<h1 class='title'>Hello World</h1>
+<p>Hello {name} </p>
+
+<br>
+<br>
+<br>
+
+<h1 class='title'>The Repeater</h1>
+<p>Write something</p>
+<input bind:value={text}>
+
+<p>you wrote: {text} </p>
